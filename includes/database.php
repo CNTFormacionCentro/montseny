0611b2ad4d6bff4341d120575aa3768d1b0690d4
CNTFormacionCentro <formacion@centro.cnt.es>
2026-04-25 20:59:33 +0200

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -7,26 +7,25 @@ function montseny_crear_tablas() {
     global $wpdb;
     $tabla = $wpdb->prefix . 'montseny_afiliados';
     
-    // Solo lo indispensable:
     $sql = "CREATE TABLE $tabla (
         id mediumint(9) NOT NULL AUTO_INCREMENT,
         user_id bigint(20) UNSIGNED NOT NULL,
         genero varchar(20),
-        fecha_nacimiento varchar(50), -- Edad o Fecha
+        fecha_nacimiento varchar(50),
         telefono varchar(20),
         direccion_cifrada text,
         iban_cifrado text,
         ramo varchar(100),
         etiquetas text,
-        observaciones text, -- Campo abierto de la columna K del Excel
+        observaciones text,
         fecha_alta date,
+        codigo_interno varchar(50),
         PRIMARY KEY  (id)
     ) {$wpdb->get_charset_collate()};";
 
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
     dbDelta( $sql );
     
-    // Clave secreta para el cifrado
     if ( ! get_option( 'montseny_secret_key' ) ) {
         update_option( 'montseny_secret_key', wp_generate_password( 64, true, true ) );
     }
