643e21a0ce3aa72cfb5e2cda9e9959c09b44a16e
CNTFormacionCentro <formacion@centro.cnt.es>
2026-04-25 21:05:32 +0200

    Update montseny.php

--- montseny.php
+++ montseny.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Montseny
 Plugin URI: https://ciudadreal.cnt.es
-Description: Gestión sindical v3.2 - RECUPERACIÓN DE CAMPOS Y LÓGICA SINDICAL.
-Version: 3.2
+Description: Gestión sindical v3.3 - Ramos Oficiales y Grabación de datos corregida.
+Version: 3.3
 Author: Montseny Project
 */
 
@@ -20,12 +20,10 @@ require_once MONTSENY_PATH . 'includes/app-ui.php';
 require_once MONTSENY_PATH . 'includes/gestion-ui.php';
 
 add_action('template_redirect', function() {
-    $request_uri = $_SERVER['REQUEST_URI'];
-    if ( strpos($request_uri, '/montseny') !== false ) {
+    if ( strpos($_SERVER['REQUEST_URI'], '/montseny') !== false ) {
         status_header(200);
         global $wp_query; $wp_query->is_404 = false;
-
-        if ( strpos($request_uri, '/montseny/gestion') !== false ) {
+        if ( strpos($_SERVER['REQUEST_URI'], '/montseny/gestion') !== false ) {
             if ( !current_user_can('montseny_tesorero') && !current_user_can('manage_options') ) {
                 wp_redirect(site_url('/montseny/')); exit;
             }
