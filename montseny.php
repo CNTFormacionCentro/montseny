7d0d54fa738d4d933ff24a8fea9a5147429ab9f9
CNTFormacionCentro <formacion@centro.cnt.es>
2026-04-25 20:24:57 +0200

    Update montseny.php

--- montseny.php
+++ montseny.php
@@ -2,21 +2,19 @@
 /*
 Plugin Name: Montseny
 Plugin URI: https://ciudadreal.cnt.es
-Description: Gestión sindical modular v2.8 - Carnet Digital Identitario (Réplica Física).
-Version: 2.8
+Description: Gestión sindical modular v2.9 - Estabilidad, Noticias con imagen y Cambio de Rol.
+Version: 2.9
 Author: Montseny Project
 */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
-
 define( 'MONTSENY_PATH', plugin_dir_path( __FILE__ ) );
 
-// Cargamos todos los módulos
 require_once MONTSENY_PATH . 'includes/updater.php';
 require_once MONTSENY_PATH . 'includes/database.php';
 require_once MONTSENY_PATH . 'includes/security.php';
 require_once MONTSENY_PATH . 'includes/roles.php';
 require_once MONTSENY_PATH . 'includes/news.php';
-require_once MONTSENY_PATH . 'includes/card-ui.php'; // Diseño del carnet
-require_once MONTSENY_PATH . 'includes/app-ui.php';  // Interfaz del afiliado
-require_once MONTSENY_PATH . 'includes/gestion-ui.php'; // Interfaz del tesorero
+require_once MONTSENY_PATH . 'includes/card-ui.php';
+require_once MONTSENY_PATH . 'includes/app-ui.php';
+require_once MONTSENY_PATH . 'includes/gestion-ui.php';
